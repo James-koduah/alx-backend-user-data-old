2afa19164ca9cb1313e2964a7b24faf9dc2ca483
James Koduah <jameskoduah171@gmail.com>
2023-11-06 19:12:58 +0000

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,6 +8,15 @@ class Auth():
     """An authentication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require_auth return false"""
+        if path[-1] != '/':
+            path = path + '/'
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if path in excluded_paths:
+            return False
+
         return False
 
     def authorization_header(self, request=None) -> str:
