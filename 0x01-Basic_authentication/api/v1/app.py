a93a93809c6f65e5e3abac9627a0e82af74de4eb
James Koduah <jameskoduah171@gmail.com>
2023-11-06 18:41:44 +0000

    task 1: pycodestyle

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -26,8 +26,8 @@ def request_unauthorized(error) -> str:
     """Unauthorized request"""
     return jsonify({"error": "Unauthorized"}), 401
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-
