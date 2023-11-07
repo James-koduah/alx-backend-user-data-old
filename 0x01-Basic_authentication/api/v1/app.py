ac41ad7e0c1f219ee641ca2ca589eb08365741bf
James Koduah <jameskoduah171@gmail.com>
2023-11-07 13:45:47 +0000

    task 6: pycodestyle

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,6 +21,7 @@ if env_auth == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
+
 @app.before_request
 def before_request():
     """run before any other thing"""
