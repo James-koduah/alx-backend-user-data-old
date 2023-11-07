ece10711cd7cb030c0db0981bfd64e8009345eb0
James Koduah <jameskoduah171@gmail.com>
2023-11-07 14:31:30 +0000

    task 8: first

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """a module that implements a Basic auth"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -13,6 +14,21 @@ class BasicAuth(Auth):
             return None
         if isinstance(authorization_header, str) is False:
             return None
-        if not re.search("^Basic ", authorization_header):
+        if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """Decode a base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if isinstance(base64_authorization_header, str) is False:
+            return None
+        try:
+            return base64.b64decode(
+                    base64_authorization_header.encode('utf-8')
+                    ).decode('utf-8')
+        except Exception:
+            return None
